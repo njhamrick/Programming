12bee78cb0e6125def9e2a0d05c1ad92fe00886e
njhamrick <njudithhamrick@gmail.com>
2026-08-28 01:58:12 -0600

    added content for project0 using variables

---
+++ Week2/project0.py
@@ -0,0 +1,13 @@
+print("Do you have a cat?")
+answer = input("Enter yes or no:")
+if answer == "yes":
+    print("I love cats!")
+else:
+    print("Maybe you should get a cat..")
+
+#Used w3schools to learn how to use if/else statements#
+
+
+print("How old are you?")
+age = input("Enter your age:")
+print(f"Wow! You're {age} years old. That's old!")
