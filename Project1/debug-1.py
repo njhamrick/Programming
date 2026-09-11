1c6939b6c2e5ad04dba38f170586f382e1bef0cb
njhamrick <njudithhamrick@gmail.com>
2026-09-11 02:17:59 -0600

    added content, variables/input for Project 1. Fixed error in Debug

--- Project1/debug-1.py
+++ Project1/debug-1.py
@@ -4,11 +4,26 @@
 # This program should add three numbers together.
 # Add your own comments discussing what you did and why you did it
 
-total = 0
-num1 = input("What's the first number? >")
-total = num1
-num2 = input("What's the second number? >")
-total = num2
-num3 = input("What's the third number? >")
-total = num3
+# Debug Exercise 2
+# This program should add three numbers together.
+# total = 0
+# num1 = input("What's the first number? >")
+# total = num1
+# num2 = input("What's the second number? >")
+# total = num2
+# num3 = input("What's the third number? >")
+# total = num3
+# print(f"Total is: {total}")
+
+
+# I changed each input to a float so Python will treat the user input as numbers
+# instead of strings. The terminal will ask each question one at a time, storing
+# each number input provided.
+num1 = float(input("What's the first number? >"))
+num2 = float(input("What's the second number? >"))
+num3 = float(input("What's the third number? >"))
+#  I removed each original total, and added a total so that all three numbers
+# would be added together
+total = (num1)+(num2)+(num3)
+# This prints the final total after the three numbers have been added together.
 print(f"Total is: {total}")
--- Project1/program2.py
+++ Project1/program2.py
@@ -4,3 +4,15 @@
 #  on a separate page. Your project does not have to follow these examples 
 # precisely (you can make your own thing, if you really want) but if you're 
 # struggling to come up with an idea I would suggest you follow the examples.
+
+
+# I changed each input to a float so Python will treat the user input as numbers
+# instead of strings. The terminal will ask each question one at a time, storing
+# each number input provided.
+desired_hours=float(input("What are your desired hours of sleep? "))
+hours_slept=float(input("How many hours did you sleep last night? "))
+# sleep_debt is the total we are looking for, this subtracts the desired_hours 
+# by the hours_slept
+sleep_debt=(desired_hours)-(hours_slept)
+# This prints the sleep_debt (total)
+print(f"You are currently owed {sleep_debt} hours of sleep.")
