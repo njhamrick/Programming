a00ec6bead8c7ddbf0f3583b39cba91a7766cbd9
Nikki Hamrick <njudithhamrick@gmail.com>
2026-09-25 06:08:29 -0600

    added comments to debug
    Removed quotes around 5 and 10 for integer comparison and simplified the if statement.

--- Project2/debug-2.py
+++ Project2/debug-2.py
@@ -4,7 +4,19 @@
 # This has two parts; read the comments and don't be afraid to
 # contact me on Canvas or email if you have questions.
 
+# user_val = int(input("Enter a whole number. >"))
+# # Part 1: Discover why this condition doesn't work and fix it.
+# # Part 2: (Don't do this until part 1 is done)
+# # We're testing the same variable twice, to make sure it falls
+# # between a range of values. Rewrite the condition to make this simpler.
+# if user_val > "5" and user_val < "10":
+# print(f"{user_val} is less than 10, but greater than 5.")
+# else:
+# print(f"{user_val} falls outside the bounds we're looking for.")
 
+
+#Removed the "" around the 5 and 10 since we are comparing integers to integers
+#Simplified the two comparisions in the if statement
 user_val = int(input("Enter a whole number. >"))
 # Part 1: Discover why this condition doesn't work and fix it.
 # Part 2: (Don't do this until part 1 is done)
